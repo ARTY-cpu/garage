f754ba9cf6326d1988fe7d9b695bbff6e635375e
Arthur Magnette <amagnette@magicsoftware.com>
2023-07-01 13:47:05 +0200

    formulaire combo box termine

--- php/get_model.php
+++ php/get_model.php
@@ -1,11 +1,24 @@
 <?php
 // Connexion à la base de données
+$servername = "bf2229608-001.eu.clouddb.ovh.net:35609";
+$username = "Garage_Admin";
+$password = "GroupeAdmin80";
+$database = "db_garage";
+
+// Create connection
+$connection = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
 
 // Récupération de la catégorie de véhicule sélectionnée depuis la requête AJAX
-$vehicleCategory = $_GET['vehicleCategory'];
+$vehicleCategory = $_POST['vehicle-category'];
 
 // Exécution de la requête SQL pour récupérer les modèles correspondants à la catégorie de véhicule
-$query = "SELECT model FROM vehicle_models WHERE category = '$vehicleCategory'";
+$query = "SELECT id,marque, modele, annee, couleur FROM voitures WHERE dispo = 1 AND classe = '$vehicleCategory'";
 $result = mysqli_query($connection, $query);
 
 // Création d'un tableau pour stocker les modèles
@@ -13,10 +26,18 @@ $models = array();
 
 // Parcours des résultats et ajout des modèles au tableau
 while ($row = mysqli_fetch_assoc($result)) {
-    $models[] = $row['model'];
+    $modelData = array(
+        'id' => $row['id'],
+        'marque' => $row['marque'],
+        'modele' => $row['modele'],
+        'annee' => $row['annee'],
+        'couleur' => $row['couleur']
+    );
+    $models[] = $modelData;
 }
 
 // Fermeture de la connexion à la base de données
+$connection->close();
 
 // Envoi des résultats au format JSON
 echo json_encode($models);
